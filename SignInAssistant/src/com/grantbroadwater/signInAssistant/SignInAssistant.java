d7cf43b44335cab618a5570a354b6f61ed7a2bd8
Grant Broadwater <broadwater.grant@gmail.com>
2015-03-20 21:23:23 -0500

    Create Main Class

---
+++ SignInAssistant/src/com/grantbroadwater/signInAssistant/SignInAssistant.java
@@ -0,0 +1,14 @@
+package com.grantbroadwater.signInAssistant;
+
+public class SignInAssistant {
+
+	public SignInAssistant() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
