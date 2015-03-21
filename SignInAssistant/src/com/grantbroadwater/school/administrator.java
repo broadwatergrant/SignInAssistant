6e0e1129a8152b5d2888877d4fd846e5511dd070
Grant Broadwater <broadwater.grant@gmail.com>
2015-03-20 21:47:10 -0500

    Create Administrator subclass of Person

---
+++ SignInAssistant/src/com/grantbroadwater/school/administrator.java
@@ -0,0 +1,13 @@
+package com.grantbroadwater.school;
+
+public class administrator extends Person {
+
+	public administrator() {
+		super();
+	}
+
+	public administrator(String name, String pin) {
+		super(name, pin);
+	}
+
+}
