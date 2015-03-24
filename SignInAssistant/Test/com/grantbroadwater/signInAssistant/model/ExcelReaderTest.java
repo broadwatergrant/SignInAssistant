d93bd8428292ef7c0109ac6402526fb710998323
Grant Broadwater <broadwater.grant@gmail.com>
2015-03-23 21:50:54 -0500

    Prepare ExcelReader class to be tested

---
+++ SignInAssistant/Test/com/grantbroadwater/signInAssistant/model/ExcelReaderTest.java
@@ -0,0 +1,14 @@
+package com.grantbroadwater.signInAssistant.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExcelReaderTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
