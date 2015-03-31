61869aa396b61d06c74134fdc258f5b6cc892d18
Grant Broadwater <broadwater.grant@gmail.com>
2015-03-31 16:49:26 -0500

    Create SignInSheet object

---
+++ SignInAssistant/src/com/grantbroadwater/signInAssistant/model/SignInSheet.java
@@ -0,0 +1,39 @@
+package com.grantbroadwater.signInAssistant.model;
+
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+
+import com.grantbroadwater.school.Status;
+import com.grantbroadwater.school.Student;
+
+public class SignInSheet {
+
+	private ArrayList<Student> list;
+	
+	public SignInSheet() {
+		list = new ArrayList<Student>();
+	}
+	
+	public void signStudentIn(Student s){
+		list.add(s);
+		s.setTimeIn(new GregorianCalendar());
+		s.setStatus(Status.IN);
+	}
+	
+	public void signStudentOut(Student s){
+		s.setTimeOut(new GregorianCalendar());
+		s.setStatus(Status.OUT);
+	}
+	
+	public void punchStudent(Student s){
+		if(s.getStatus() == Status.IN)
+			signStudentOut(s);
+		if(s.getStatus() == Status.OUT)
+			signStudentIn(s);
+	}
+	
+	public Student[] getSignInSheet(){
+		return list.toArray(new Student[list.size()]);
+	}
+
+}
