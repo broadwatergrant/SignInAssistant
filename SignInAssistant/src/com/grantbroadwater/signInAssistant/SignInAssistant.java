61ba4e5eb8d5a09cab3e1b416e1d2e75c1c56ab9
Grant Broadwater <broadwater.grant@gmail.com>
2015-03-31 21:57:13 -0500

    Create model, view, and controller objects for the SignInAssistant

--- SignInAssistant/src/com/grantbroadwater/signInAssistant/SignInAssistant.java
+++ SignInAssistant/src/com/grantbroadwater/signInAssistant/SignInAssistant.java
@@ -1,10 +1,43 @@
 package com.grantbroadwater.signInAssistant;
 
+import com.grantbroadwater.signInAssistant.controller.Controller;
+import com.grantbroadwater.signInAssistant.model.Model;
+import com.grantbroadwater.signInAssistant.view.View;
+
 public class SignInAssistant {
 
+	private static Model model;
+	private static View view;
+	private static Controller controller;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		model = new Model();
+		view = new View();
+		controller = new Controller();
+	}
 
+	public static Model getModel() {
+		return model;
+	}
+
+	public static void setModel(Model model) {
+		SignInAssistant.model = model;
+	}
+
+	public static View getView() {
+		return view;
+	}
+
+	public static void setView(View view) {
+		SignInAssistant.view = view;
+	}
+
+	public static Controller getController() {
+		return controller;
+	}
+
+	public static void setController(Controller controller) {
+		SignInAssistant.controller = controller;
 	}
 
 }
