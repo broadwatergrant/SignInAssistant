e4f2de6d8327815b41b6c3deb317784a957f663c
Grant Broadwater <broadwater.grant@gmail.com>
2015-03-31 19:32:37 -0500

    Create and test SIAFrame

--- SignInAssistant/Test/com/grantbroadwater/signInAssistant/view/SIAFrameTest.java
+++ SignInAssistant/Test/com/grantbroadwater/signInAssistant/view/SIAFrameTest.java
@@ -1,6 +1,7 @@
 package com.grantbroadwater.signInAssistant.view;
 
-import static org.junit.Assert.*;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import org.junit.Test;
 
@@ -8,7 +9,39 @@ public class SIAFrameTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		SIAFrame frame = new SIAFrame();
+		
+		JPanel pnl1 = new JPanel();
+		pnl1.add(new JLabel("Panel 1"));
+		JPanel pnl2 = new JPanel();
+		pnl2.add(new JLabel("Panel 2"));
+		
+		JPanel[] panels = {pnl1, pnl2};
+		String[] panelNames = {"panel1", "panel2"};
+		
+		frame.setPanels(panels, panelNames);
+		frame.showPanel(0);
+		frame.setSize(300, 300);
+		frame.setVisible(true);
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		frame.showPanel(1);
+		frame.setSize(400, 400);
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 	}
 
 }
--- SignInAssistant/src/com/grantbroadwater/signInAssistant/view/SIAFrame.java
+++ SignInAssistant/src/com/grantbroadwater/signInAssistant/view/SIAFrame.java
@@ -1,9 +1,10 @@
 package com.grantbroadwater.signInAssistant.view;
 
-import java.awt.Dimension;
+import java.awt.CardLayout;
 import java.awt.HeadlessException;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class SIAFrame extends JFrame {
 
@@ -11,14 +12,39 @@ public class SIAFrame extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
-	public static final int WIDTH = 800, HEIGHT = 600;
 	
-	public SIAFrame() throws HeadlessException {
+	private JPanel contentPane;
+	private CardLayout cardLayout;
+	private String[] panelNames;
+	
+	public SIAFrame(){
 		super("Sign In Assistant");
-		setSize(new Dimension(WIDTH, HEIGHT));
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
+		cardLayout = new CardLayout();
+		contentPane = new JPanel(cardLayout);
+		this.getContentPane().add(contentPane);
+	}
+	
+	public SIAFrame(JPanel[] panels, String[] panelNames) throws HeadlessException {
+		this();
+		setPanels(panels, panelNames);
+	}
+	
+	public void setPanels(JPanel[] panels, String[] panelNames){
+		this.panelNames = panelNames;
+		
+		for (int i = 0; i < panels.length; i++) {
+			contentPane.add(panels[i], panelNames[i]);
+		}
+	}
+	
+	public void showPanel(int panelNameIndex){
+		showPanel(panelNames[panelNameIndex]);
+	}
+	
+	public void showPanel(String panelName){
+		cardLayout.show(contentPane, panelName);
 	}
 
 }
