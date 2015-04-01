0fdef2805d884f5c6e1105dff95cbaa003c8282d
Grant Broadwater <broadwater.grant@gmail.com>
2015-03-31 19:43:39 -0500

    Create an abstract JPanel subclass that has a getCardLayoutName

---
+++ SignInAssistant/src/com/grantbroadwater/signInAssistant/view/GPanel.java
@@ -0,0 +1,29 @@
+package com.grantbroadwater.signInAssistant.view;
+
+import java.awt.LayoutManager;
+
+import javax.swing.JPanel;
+
+public abstract class GPanel extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private static int instanceCount = 0;
+
+	public GPanel() {
+		super();
+		instanceCount++;
+	}
+
+	public GPanel(LayoutManager layout) {
+		super(layout);
+		instanceCount++;
+	}
+
+	public final String getCardLayoutName(){
+		return this.getClass().getName() + instanceCount;
+	}
+
+}
