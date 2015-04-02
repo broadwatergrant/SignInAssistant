72dbe1e8086caf4f5d47400c31f4ef0e11c2d855
Grant Broadwater <broadwater.grant@gmail.com>
2015-04-01 20:56:26 -0500

    Create method to be called by ActionListener when a valid administrator has signed in

--- SignInAssistant/src/com/grantbroadwater/signInAssistant/controller/Controller.java
+++ SignInAssistant/src/com/grantbroadwater/signInAssistant/controller/Controller.java
@@ -1,9 +1,15 @@
 package com.grantbroadwater.signInAssistant.controller;
 
+import com.grantbroadwater.school.Administrator;
+
 public class Controller {
 
 	public Controller() {
 		// TODO Auto-generated constructor stub
 	}
 
+	protected void signInAdmin(Administrator admin){
+		
+	}
+	
 }
