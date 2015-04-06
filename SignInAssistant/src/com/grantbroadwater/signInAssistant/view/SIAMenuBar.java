55ec19381dcc8356b1fe67325b06aa06ee4b7e18
Grant Broadwater <broadwater.grant@gmail.com>
2015-04-06 17:15:47 -0500

    Create a menubar class

---
+++ SignInAssistant/src/com/grantbroadwater/signInAssistant/view/SIAMenuBar.java
@@ -0,0 +1,84 @@
+package com.grantbroadwater.signInAssistant.view;
+
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class SIAMenuBar extends JMenuBar {
+
+	private static final long serialVersionUID = 1L;
+
+	private JMenu signInAssistantMenu, showMenu;
+	private JMenuItem showInquire, showSignInSheet, showData;
+	private JMenuItem signOut, startStopSignIn, close;
+	
+	public SIAMenuBar() {
+		super();
+		
+		signInAssistantMenu = new JMenu("Sign In Assistant");
+		signInAssistantMenu.setMnemonic(KeyEvent.VK_A);
+		signInAssistantMenu.getAccessibleContext().setAccessibleDescription(
+				"Main actions of the Sign In Assistant");
+		
+		startStopSignIn = new JMenuItem();
+		signInAssistantMenu.add(startStopSignIn);
+		
+		signOut = new JMenuItem("Sign Out");
+		signInAssistantMenu.add(signOut);
+		
+		signInAssistantMenu.addSeparator();
+		
+		close = new JMenuItem("Close");
+		signInAssistantMenu.add(close);
+		
+		add(signInAssistantMenu);
+		
+		showMenu = new JMenu("Show");
+		showMenu.setMnemonic(KeyEvent.VK_S);
+		showMenu.getAccessibleContext().setAccessibleDescription(
+				"Select which Screen to show");
+		
+		showInquire = new JMenuItem("Inquire");
+		showMenu.add(showInquire);
+		
+		showSignInSheet = new JMenuItem("Sign In Sheet");
+		showMenu.add(showSignInSheet);
+		
+		showData = new JMenuItem("Data");
+		showMenu.add(showData);
+		
+		add(showMenu);
+	}
+
+	public void setStartStopSignInTest(String text){
+		startStopSignIn.setText(text);
+	}
+	
+	public void addStartStopSignInSelectedFromMenuBarListener(ActionListener listener){
+		startStopSignIn.addActionListener(listener);
+	}
+	
+	public void addSignOutSelectedFromMenuBarListener(ActionListener listener){
+		signOut.addActionListener(listener);
+	}
+	
+	public void addCloseSelectedFromMenuBarListener(ActionListener listener){
+		close.addActionListener(listener);
+	}
+	
+	public void addInquireSelectedFromMenuBarListener(ActionListener listener){
+		showInquire.addActionListener(listener);
+	}
+	
+	public void addSignInSheetSelectedFromMenuBarListener(ActionListener listener){
+		showSignInSheet.addActionListener(listener);
+	}
+	
+	public void addDataSelectedFromMenuBarListener(ActionListener listener){
+		showData.addActionListener(listener);
+	}
+	
+}
