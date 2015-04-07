0d535b7e8cf400d6157fa51bd64be44a87de9563
Grant Broadwater <broadwater.grant@gmail.com>
2015-04-06 23:38:57 -0500

    Set the default look and feel

--- SignInAssistant/src/com/grantbroadwater/signInAssistant/SignInAssistant.java
+++ SignInAssistant/src/com/grantbroadwater/signInAssistant/SignInAssistant.java
@@ -1,5 +1,7 @@
 package com.grantbroadwater.signInAssistant;
 
+import javax.swing.UIManager;
+
 import com.grantbroadwater.signInAssistant.controller.Controller;
 import com.grantbroadwater.signInAssistant.model.Model;
 import com.grantbroadwater.signInAssistant.view.View;
@@ -11,6 +13,10 @@ public class SignInAssistant {
 	private static Controller controller;
 	
 	public static void main(String[] args) {
+		try{
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		}catch(Exception e){}
+		
 		controller = new Controller();
 		controller.createModelAndView();
 		
