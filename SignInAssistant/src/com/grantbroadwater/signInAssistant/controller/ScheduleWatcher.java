3a10b5ce2fd10f4dee213cb8dc44c66f537a0833
Grant Broadwater <broadwater.grant@gmail.com>
2015-04-08 17:17:22 -0500

    Fix schedule watcher to know schedule at start rather than after the first minute

--- SignInAssistant/src/com/grantbroadwater/signInAssistant/controller/ScheduleWatcher.java
+++ SignInAssistant/src/com/grantbroadwater/signInAssistant/controller/ScheduleWatcher.java
@@ -7,53 +7,51 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import com.grantbroadwater.signInAssistant.model.Model;
-import com.grantbroadwater.util.Log;
 
 public class ScheduleWatcher extends Timer {
 
 	private Model model;
 	private Controller controller;
 	private WatchScheduleTask watchScheduleTask;
-	
+
 	public ScheduleWatcher(Model model, Controller controller) {
 		this.model = model;
 		this.controller = controller;
 	}
 
-	
-	public void startWatchingSchedule(){
-		watchScheduleTask = new WatchScheduleTask(model, controller);
-		
+	public void startWatchingSchedule() {
+		watchScheduleTask = new WatchScheduleTask(model, controller,
+				model.getCurrentHour());
+
 		GregorianCalendar gc = new GregorianCalendar();
 		gc.set(Calendar.MINUTE, gc.get(Calendar.MINUTE) + 1);
 		gc.set(Calendar.SECOND, 0);
 		gc.set(Calendar.MILLISECOND, 0);
 		Date date = gc.getTime();
-		
+
 		schedule(watchScheduleTask, date, 60000);
 	}
-	
+
 }
 
-class WatchScheduleTask extends TimerTask{
+class WatchScheduleTask extends TimerTask {
 
 	private Model model;
 	private Controller controller;
 	private int currentHour;
-	
-	public WatchScheduleTask(Model model, Controller controller) {
+
+	public WatchScheduleTask(Model model, Controller controller, int currentHour) {
 		this.model = model;
 		this.controller = controller;
+		this.currentHour = currentHour;
 	}
-	
+
 	@Override
 	public void run() {
-		new Log(Log.LogType.DEBUG, "Here: "+currentHour+" model: "+model.getCurrentHour());
-		if(currentHour != model.getCurrentHour()){
+		if (currentHour != model.getCurrentHour()) {
 			controller.autoSignOutStudents();
 			currentHour = model.getCurrentHour();
 		}
 	}
-	
-}
 
+}
