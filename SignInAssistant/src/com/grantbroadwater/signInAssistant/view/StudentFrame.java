dec29624ab7cb4a92af62430078f8d8b4bd3415f
Grant Broadwater <broadwater.grant@gmail.com>
2015-04-19 19:30:23 -0500

    Make student frame center in second monitor

--- SignInAssistant/src/com/grantbroadwater/signInAssistant/view/StudentFrame.java
+++ SignInAssistant/src/com/grantbroadwater/signInAssistant/view/StudentFrame.java
@@ -1,6 +1,12 @@
 package com.grantbroadwater.signInAssistant.view;
 
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Point;
+
 import javax.swing.JFrame;
+
+import com.grantbroadwater.util.Log;
 
 public class StudentFrame extends JFrame {
 
@@ -14,6 +20,22 @@ public class StudentFrame extends JFrame {
 		this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE); 
 		this.setMinimumSize(studentPanel.getPreferredSize());
 		this.setMaximumSize(studentPanel.getPreferredSize());
+		
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		GraphicsDevice[] gd = ge.getScreenDevices();
+		
+		if(gd.length >= 0){
+			setLocation(getCenterOf(gd[gd.length - 1]));
+		}else{
+			new Log(Log.LogType.ERROR, "No Screens found");
+		}
+		
+	}
+	
+	public Point getCenterOf(GraphicsDevice screen){
+		int x = screen.getDefaultConfiguration().getBounds().width / 2 - this.getWidth() / 2;
+		int y = screen.getDefaultConfiguration().getBounds().height / 2 - this.getHeight() / 2;
+		return new Point(x, y);
 	}
 	
 	public StudentPanel getStudentPanel(){
